84eb9f93fed2180d4f198c4e66614e3088536f66
paroom6 <opwqer3729@gmail.com>
2024-02-16 12:47:16 +0900

    feat: 0216 수업자료

---
+++ src/main/java/com/study/product/dto/InsertUserReqDto.java
@@ -0,0 +1,14 @@
+package com.study.product.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class InsertUserReqDto {
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+
+}
